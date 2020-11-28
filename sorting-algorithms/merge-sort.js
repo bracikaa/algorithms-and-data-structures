449add109a5b3ec7dce2918d5b7aa828fabc00a1
Mehmed <mehmed_duhovic@hotmail.com>
2020-11-28 22:17:52 +0100

    merge sort

---
+++ sorting-algorithms/merge-sort.js
@@ -0,0 +1,42 @@
+function mergeSort(arr) {
+  debugger;
+  if (arr.length <= 1) {
+    return arr;
+  } else {
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+  }
+}
+
+function merge(arr1, arr2) {
+  let results = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  console.log(results);
+  return results;
+}
+
+mergeSort([2, 1, 10, 100, 4, 99, 10, 50]);
