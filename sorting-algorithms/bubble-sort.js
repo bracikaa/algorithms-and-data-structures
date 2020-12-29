949f59e362800a83484c9bcb3b7574112a4f7110
Mehmed <mehmed_duhovic@hotmail.com>
2020-12-29 15:45:53 +0100

    bubble sort optimized

--- sorting-algorithms/bubble-sort.js
+++ sorting-algorithms/bubble-sort.js
@@ -1,13 +1,20 @@
 function bubbleSort(arr) {
-    for(let i = arr.length; i > 0; i--) {
-        for(let j = 0; j < i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-            }
-        }
+  let swapHappened;
+  for (let i = arr.length; i > 0; i--) {
+    swapHappened = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapHappened = false;
+      }
     }
+    if (swapHappened) {
+      break;
+    }
+  }
 
-    return arr;
+  return arr;
 }
 
 bubbleSort([15, 6, 4, 18, 7, 13, 1]);
+bubbleSort([2, 4, 6, 5, 7, 9, 12]);
