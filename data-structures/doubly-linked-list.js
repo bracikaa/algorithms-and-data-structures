b0c581a2d663172280bb9db6228bb5def22b132d
Mehmed <mehmed_duhovic@hotmail.com>
2021-01-19 00:00:14 +0100

    heaad and unshift

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -1,55 +1,115 @@
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.prev = null;
-        this.next = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
 }
 
 class DoublyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
     }
 
-    push(value) {
-        let newNode = new Node(value);
+    this.length++;
+    return this;
+  }
 
-        if(this.length === 0) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-            this.tail = newNode;
-        }
-
-        this.length++;
-        return this;
+  pop() {
+    if (this.length === 0) {
+      return undefined;
     }
 
-    pop() {
-        if(this.length === 0) {
-            return undefined;
-        }
+    let poppedNode = this.tail;
 
-        let poppedNode = this.tail;
-
-        if(this.length === 1) {
-            this.head = null;
-            this.tail = null;
-        } else {
-            this.tail = poppedNode.prev;
-            this.tail.next = null;
-            poppedNode.prev = null;
-        }
-
-        this.length--;
-        return poppedNode;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
     }
+
+    this.length--;
+    return poppedNode;
+  }
+
+  shift() {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    let oldHead = this.head;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+
+    this.length--;
+    return oldHead;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    if (index <= this.length / 2) {
+      let count = 0;
+      let current = this.head;
+      while (count != index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+        let count = this.length - 1;
+        let current = this.tail;
+        while(count != index) {
+            current = current.prev;
+            count--;
+        }
+    }
+
+    return current;
+  }
 }
 
 let list = new DoublyLinkedList();
-list.push(99);
-list.push(100);
+list.unshift(2);
+list.unshift(1);
+list.unshift(0);
+list.push(3);
+list.push(4);
+let findNode = list.get(3);
