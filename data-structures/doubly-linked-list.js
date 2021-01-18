a2a8942929b4aeda11b8e4d322fe84302e271339
Mehmed <mehmed_duhovic@hotmail.com>
2021-01-18 23:21:36 +0100

    doubly linked list start

---
+++ data-structures/doubly-linked-list.js
@@ -0,0 +1,55 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(value) {
+        let newNode = new Node(value);
+
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if(this.length === 0) {
+            return undefined;
+        }
+
+        let poppedNode = this.tail;
+
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+
+        this.length--;
+        return poppedNode;
+    }
+}
+
+let list = new DoublyLinkedList();
+list.push(99);
+list.push(100);
